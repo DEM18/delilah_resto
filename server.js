f96e359fda2102b0ee2c48671daf8268d3ac0c95
DEM18 <morenodanielaeliana@gmail.com>
2020-04-22 14:34:21 -0300

    Create credentials validation middleware for login endpoint

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const login = require('./repository/users_repository');
 const bodyParser = require('body-parser');
 const server = express();
+/* const NULL = "null"; */
 
 server.use(bodyParser.json());
 
@@ -9,16 +10,28 @@ server.listen(3000, () => {
     console.log('iniciando servidor');
 });
 
-/* login.getUsernameAndPassword( 'gomezl1', '12548930'); */
-
-//APIs
 
 //-----Login-----
 
-server.post( '/login', ( req, res ) => {
-    console.log( req.body );
-    if( login.getUsernameAndPassword( req.body ) ) {
-        console.log("PUEDE INICIAR SESION");
-    } else console.log("NO PUEDE INICIAR SESION");
-
+server.post( '/login', validateCredentials, ( req, res ) => {
+    const { emailUsername, password } = req.body;
+    if( res.statusCode === 200 ) {
+        login.getUsernameAndPassword( emailUsername, password );
+    }
 });
+
+function validateCredentials( req, res, next ) {
+    const { emailUsername, password } = req.body;
+    console.log( emailUsername, password );
+    if( emailUsername === undefined || emailUsername  === null || emailUsername === "" ) {
+        console.log('BAD REQUEST');
+        res.statusCode = 400;
+    } else if( password === undefined || password === null || password === "" ){
+        console.log('BAD REQUEST');
+        res.statusCode = 400;
+    } else {
+        console.log('OK REQUEST');  
+        res.statusCode = 200;
+        next();
+    }
+}
