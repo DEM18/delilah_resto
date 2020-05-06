c8c526a7e1cb6247001e7c656d431831ff93a80a
DEM18 <morenodanielaeliana@gmail.com>
2020-05-06 15:14:55 -0300

    Create createproduct endpoint for products

--- routes/product.js
+++ routes/product.js
@@ -1,9 +1,25 @@
-server.get('/favorite', ( res ) => {
+const productController = require('../controllers/product');
+const express = require('express');
+const router_product = express.Router();
+
+router_product.get('/favorite', ( res ) => {
     res.statusCode = 200;
     res.json(product.getFavoriteProducts());
 })
 
-server.get('/product', ( res ) => {
+router_product.get('/product', ( res ) => {
     res.statusCode = 200;
     res.json( product.getProducts() );
 });
+
+router_product.post('/createproduct', async ( req, res ) => {
+    console.log(req.body);
+    let saveProduct = await productController.insertProduct( req.body );
+
+    if( saveProduct ) {
+        res.statusCode = 200;
+        res.json("product added sucessfully");
+    }
+});
+
+module.exports = router_product;
